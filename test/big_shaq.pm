82c37b5151d740c06832ceee0f2e7285014b260f
xaver <xaverfischer@mail.de>
2018-07-21 22:21:26 +0200

    Added more Tests, Fixed a bug which caused incorrect checking of the script_start token and parsing a read_stdio token now uses the correct type

--- test/big_shaq.pm
+++ test/big_shaq.pm
@@ -35,7 +35,7 @@ sub test {
         my $obj = big_shaq::new();
         my $data = $obj->parse_print($valid_call, $line_number);
 
-        Test::More::is_deeply($expected_data, $data, "Parsing a 'Print' Call");
+        Test::More::is_deeply($expected_data, $data, "big_shaq::parse_print() - Parsing a 'Print' Call");
     }
 
     # Check if parsing of a Function call works
@@ -49,7 +49,7 @@ sub test {
             );
         my $obj = big_shaq::new();
         my $data = $obj->parse_func_call($valid_call, $line_number);
-        Test::More::is_deeply($expected_data, $data, "Parsing a Function Call");
+        Test::More::is_deeply($expected_data, $data, "big_shaq::parse_func_call() - Parsing a Function Call");
     }
 
     # Check if parsing of a Function definition works 
@@ -68,8 +68,110 @@ sub test {
         $obj->{'content'}[$end_func -1] = "Skidiki-pap-pap-pap-pudrrrr-boom";
         my $data = $obj->parse_func_def_start($valid_call, $line_number);
 
-        Test::More::is_deeply($expected_data, $data, "Parsing a Function Defintion");
+        Test::More::is_deeply($expected_data, $data, "big_shaq::parse_func_def_start() - Parsing a Function Defintion");
     }
+
+    # Check if appending of parsed_content works
+    {
+        my $expected = script_start->new();
+        my $obj = big_shaq::new();
+        $obj->append_parsed_content($expected);
+        my $got = $obj->{'parsed_content'}[0];
+        Test::More::is($got, $expected, "big_shaq::append_parsed_content - Appending Parsed Content");
+    }
+
+    # Check if Getting an identifier works
+    {
+        my $expected = 1;
+        my $obj = big_shaq::new();
+        my $got = $obj->get_identifier();
+
+        Test::More::is($got, $expected, "big_shaq::get_identifier() - Getting identifier and incrementing it by 1");
+    }
+
+    # Check if converting function names works
+    {
+        my $data = "pap-pap";
+        my $expected = "pap_pap";
+        my $obj = big_shaq::new();
+        my $got = $obj->convert_func_name_to_valid_perl($data);
+
+        Test::More::is($got, $expected, "big_shaq::convert_func_name_to_valid_perl() - Converting Function names");
+    }
+
+    # Check if adding a script_start token works
+    {
+        my $expected = script_start->new();
+        my $obj = big_shaq::new();
+        $obj->add_script_start();
+        my $got = pop(@{$obj->{'parsed_content'}});
+        Test::More::is_deeply($got, $expected, "big_shaq::add_script_start() - Adding a 'script_start' to parsed_content");
+    }
+
+    # Check if adding a script_end token works
+    {
+        my $expected = script_end->new();
+        my $obj = big_shaq::new();
+        $obj->add_script_end();
+        my $got = pop(@{$obj->{'parsed_content'}});
+        Test::More::is_deeply($got, $expected, "big_shaq::add_script_end() - Adding a 'script_end' to parsed_content");
+    }
+
+    # Check if checking for a 'script_start' token works
+    {
+        my $expected = '1';
+        my $obj = big_shaq::new();
+        $obj->add_script_start();
+        my $got = $obj->check_script_start();
+        Test::More::is($got, $expected, "big_shaq::check_script_start() - Checking if the first parsed token is a script_start'");
+    }
+
+    # Check if add_read_stdio works
+    {
+        my $data = 'The girl told me raw sauce';
+        my $line_number = 1;
+        my $expected = read_stdio->new(
+            line => $line_number,
+            variable_name => 'raw sauce'
+        );
+        my $obj = big_shaq::new();
+
+        $obj->add_read_stdio($data, $line_number);
+        my $got = pop(@{$obj->{'parsed_content'}});
+        Test::More::is_deeply($got, $expected, "big_shaq::add_read_stio() - Adding a read_stdio token");
+    }
+
+    # Check if Parsing of a read_stdio works
+    {
+        my $data = 'The girl told me raw sauce';
+        my $line_number = 1;
+        my $expected = read_stdio->new(
+            line => $line_number,
+            variable_name => 'raw sauce',
+        );
+
+        my $obj = big_shaq::new();
+        my $got = $obj->parse_read_stdio($data, $line_number);
+
+        Test::More::is_deeply($got, $expected, "big_shaq::parse_read_stdio - Parsing a 'Read STDIO' Statement");
+    }
+
+    {
+        my $data = 'I tell her raw sauce';
+        my $line_number = 1;
+        my $expected = call->new(
+            line            => $line_number,
+            function_name   => 'print',
+            parameter       => '\'raw sauce\'."\n"',
+        );
+
+        my $obj = big_shaq::new();
+        $obj->add_print($data, $line_number);
+        my $got = pop(@{$obj->{'parsed_content'}});
+
+        Test::More::is_deeply($got, $expected, "big_shaq::add_print - Parsing a 'Read STDIO' Statement");  
+    }
+
 
     # end testing in case this is run in standalone
     if ($test_is_run_alone) {
